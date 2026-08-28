0c096ba9d1204af96236f05f895e0d95bdda2811
gana4141 <ganeshpagar288@gmail.com>
2026-08-29 00:48:56 +0530

    updated classes

--- src/com/nit/logicals/SwapNumbers.java
+++ src/com/nit/logicals/SwapNumbers.java
@@ -1,19 +1,13 @@
 package com.nit.logicals;
-
 import java.util.Scanner;
-
-public class SwapNumbers
-{
-public static void main(String[] args)
-{
-Scanner scanner = new Scanner(System.in);
-
+public class SwapNumbers{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
 int a = 5;
 int b = 10;	
 System.out.println("Before swapping: a = " + a + ", b = " + b);
 a = a + b;
 b = a - b;
 a = a - b;
-System.out.println("After swapping: a = " + a + ", b = " + b);
-}
-}
+System.out.println("After swapping: a = " + a + ", b = " + b);}}
+
