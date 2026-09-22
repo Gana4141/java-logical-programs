fcce80be450fc834907e8ed440eacebf48502eda
gana4141 <ganeshpagar288@gmail.com>
2026-09-23 00:26:15 +0530

    "initial update "

---
+++ src/com/nit/arrays_logicals/SecondLargest.java
@@ -0,0 +1,43 @@
+package com.nit.arrays_logicals;
+
+
+
+import java.util.Scanner;
+
+public class SecondLargest {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int size = scanner.nextInt();
+
+        int[] arr = new int[size];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < size; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } 
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + secondLargest);
+
+        scanner.close();
+    }
+}
