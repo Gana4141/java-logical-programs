edbd54126bd55a2f2f6821c082defdf0027e184a
gana4141 <ganeshpagar288@gmail.com>
2026-09-26 00:26:18 +0530

    all addd the project

---
+++ src/com/nit/arrays_logicals/RemoveDuplicates.java
@@ -0,0 +1,36 @@
+package com.nit.arrays_logicals;
+
+
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 10, 30, 20, 40, 30};
+
+        System.out.println("Original Array:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("\nArray after removing duplicates:");
+
+        for (int i = 0; i < arr.length; i++) {
+
+            boolean duplicate = false;
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[i] == arr[j]) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (!duplicate) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
